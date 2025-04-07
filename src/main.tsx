03eb7f0337f10931c6fff88c8b4d0114e0d84659
egkike <egkike@gmail.com>
2025-04-07 00:02:38 -0300

    Cambio en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
