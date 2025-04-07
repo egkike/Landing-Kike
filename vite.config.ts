61fb22cf2855c1881264289897bdeab37a8bc73e
egkike <egkike@gmail.com>
2025-04-07 01:16:26 -0300

    Cambio en index.html y vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '/main.tsx': path.resolve(__dirname, 'src/main.tsx'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
