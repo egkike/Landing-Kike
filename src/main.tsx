b230e972e886cc7c3bb68a091f075270e782243d
egkike <egkike@gmail.com>
2025-04-10 20:53:26 -0300

    Cambios en vite.config.js, index.html y main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-const basename = process.env.NODE_ENV === 'production' ? '/Landing-Kike/' : '/';
+const basename = process.env.NODE_ENV === "production" ? "/Landing-Kike/" : "/";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Landing-Kike/' : '/',
-  resolve: {
-    alias: {
-      "/main.tsx": path.resolve(__dirname, "src/main.tsx"),
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
+  base: process.env.NODE_ENV === "production" ? "/Landing-Kike/" : "/",
 });
